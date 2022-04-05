e70ebd184d2689c8df865258330f573096e0038a
ppalekar <ppalekar@LT290.citi-us.com>
2022-04-05 17:24:30 +0530

    Updated TC5

--- Essen/src/test/java/com/essen/testCases/TC_PageChecker_005.java
+++ Essen/src/test/java/com/essen/testCases/TC_PageChecker_005.java
@@ -2,7 +2,9 @@ package com.essen.testCases;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -61,9 +63,6 @@ public class TC_PageChecker_005 extends BaseClass {
 		driver.findElement(By.xpath("//li[@id='LI_58']")).click();
 		driver.findElement(By.xpath("//a[@href='/RulesEngine/ViewPatientsForOpenEnrollment?menuID=329']")).click();
 
-		WebDriverWait wait2 = new WebDriverWait(driver, 15);
-		wait2.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class=\"k-loading-image\"]")));
-
 		WebDriverWait wait1 = new WebDriverWait(driver, 30);
 		wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@id='btnExportCSV']")));
 
@@ -76,18 +75,22 @@ public class TC_PageChecker_005 extends BaseClass {
 			logger.info("Medical Enrollment Validation Failed");
 			Assert.assertTrue(false);
 		}
+		WebDriverWait wait3 = new WebDriverWait(driver, 10);
+		wait3.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class=\"k-loading-image\"]")));
 	}
 
 	@Test(description = "To validate scenario of Consent>> DCE all patients", priority = 4)
 	public void consent_DCEAllPatients() throws IOException, InterruptedException {
-
-		MenuList menu = new MenuList(driver);
+		/*
+		 * driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		 * driver.findElement(By.id("LI_315")).click();
+		 * driver.findElement(By.id("LI_314")).click();
+		 */
+		MenuList menu=new MenuList(driver);
 		menu.clickConsentMenu();
 		menu.clickDce_AllPatients();
-		logger.info("Invisibility element STARTS");
 		WebDriverWait wait2 = new WebDriverWait(driver, 15);
 		wait2.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class=\"k-loading-image\"]")));
-		logger.info("Invisibility element DONE");
 
 		WebDriverWait wait1 = new WebDriverWait(driver, 30);
 		wait1.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@title='Eligible – Not Enrolled']")));
@@ -103,23 +106,52 @@ public class TC_PageChecker_005 extends BaseClass {
 		}
 	}
 
-	@Test(description = "To validate scenario of Consent>>HH_EligiblePatients", priority = 5)
-	public void HH_EligiblePatients() throws Exception {
-		MenuList menu = new MenuList(driver);
+	@Test(description="To validate scenario of Consent>>HH_EligiblePatients", priority=5)
+	public void HH_EligiblePatients() throws Exception
+	{
+		MenuList menu=new MenuList(driver);
 		menu.clickHhEligiblePatients();
-
-		WebDriverWait wait = new WebDriverWait(driver, 20);
+		
+		WebDriverWait wait=new WebDriverWait(driver, 20);
 		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//select[@id='HHConsentStatus']")));
-
-		if (driver.findElement(By.xpath("//div[@class=\"col-sm-6\"]//h4")).getText()
-				.contains("Patient List - Health Home All Patients")) {
+		
+		if(driver.findElement(By.xpath("//div[@class=\"col-sm-6\"]//h4")).getText().contains("Patient List - Health Home All Patients"))
+		{
 			Assert.assertTrue(true);
 			logger.info("Patient List - Health Home All Patients page Successfully Validated");
-		} else {
+		}
+		else
+		{
 			captureScreen(driver, "HH_EligiblePatients");
 			Assert.assertTrue(false);
 			logger.info("Patient List - Health Home All Patients validation Failed");
 		}
+		WebDriverWait wait2 = new WebDriverWait(driver, 15);
+		wait2.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class=\"k-loading-image\"]")));
+
 	}
 
+	@Test(description="To validate scenarion of RHIO consent", priority=6)
+	public void RhioConsent() throws Exception
+	{
+		MenuList menu=new MenuList(driver);
+		menu.clickRHIOConsent();
+		
+		WebDriverWait wait4=new WebDriverWait(driver, 15);
+		wait4.until(ExpectedConditions.invisibilityOfElementLocated(By.xpath("//div[@class=\"k-loading-image\"]")));
+		
+		if(driver.findElement(By.xpath("//div[@class='col-sm-6']/h4")).getText().contains("Patient List - RHIO Consent"))
+		{
+			Assert.assertTrue(true);
+			logger.info("Patient List - RHIO Consent page successfully validated");
+		}
+		else
+		{
+			captureScreen(driver, "Patient List - RHIO Consent");
+			Assert.assertTrue(false);
+			logger.info("Patient List - RHIO Consent validation failed");
+		}
+	}
 }
+
+
