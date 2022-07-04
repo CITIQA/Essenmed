43a94b88a6f01327898c73b23bb76c866b6d4347
ppalekar <ppalekar@LT290.citi-us.com>
2022-07-04 18:45:12 +0530

    SSRS report latest code

--- Essen/src/test/java/com/essen/pageObjects/SSRS.java
+++ Essen/src/test/java/com/essen/pageObjects/SSRS.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 public class SSRS {
@@ -24,6 +25,7 @@ public class SSRS {
 	@CacheLookup
 	WebElement Appointments;
 	
+	//KioskCheckInsReport
 	@FindBy(id="LI_288")
 	@CacheLookup
 	WebElement KioskCheckInsReport;
@@ -36,29 +38,54 @@ public class SSRS {
 	@CacheLookup
 	WebElement NoShowBookedAppointmentsReport;
 
+	@FindBy(how=How.ID, using="LI_323")
+	WebElement Exception;
+	
+	@FindBy(how=How.ID, using="LI_284")
+	WebElement PatientList_Exceptions;
+	
+	@FindBy(how=How.ID, using="LI_289")
+	WebElement DuplicateChart;
+	
+	@FindBy(how=How.ID, using="LI_290")
+	WebElement DuplicateSubscriberNumber;
+	
+	@FindBy(how=How.ID, using="LI_302")
+	WebElement Opt_OutList;
 	
 	
-	
-	
-	
-	
-	
-	public void KioskCheckInsReport()
+	public void KioskCheckInsReportMenu()
 	{
 		ReportsMenu.click();
 		Appointments.click();
 		KioskCheckInsReport.click();
 	}
-/*	public void WowReport()
+	public void WowReportMenu()
 	{
 		//ReportsMenu.click();
 		//Appointments.click();
 		WowReport.click();
 	}
-	public void NoShowBookedAppointmentsReport()
+	public void NoShowBookedAppointmentsReportMenu()
 	{
 		//ReportsMenu.click();
 		//Appointments.click();
 		NoShowBookedAppointmentsReport.click();
-	} */
+	} 
+	public void clickExceptionMenu()
+	{
+		Exception.click();
+	}
+	public void clickPatientList_ExceptionsMenu()
+	{
+		PatientList_Exceptions.click();
+	}
+	public void clickDuplicateChartMenu()
+	{
+		DuplicateChart.click();
+	}
+	public void clickOpt_OutListMenu()
+	{
+		Opt_OutList.click();
+	}
 }
